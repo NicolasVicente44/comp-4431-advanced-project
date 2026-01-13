c12b17f1bb02da678fa0c8bf60f03229c0c5f069
Nicolas Vicente <101988860+NicolasVicente44@users.noreply.github.com>
2026-01-13 11:40:07 -0500

    test

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "COMP 4431 Advanced Project",
+  description: "A showcase of advanced web development concepts.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} min-h-screen flex flex-col`}>
+        {children}
+      </body>
+    </html>
+  );
+}
